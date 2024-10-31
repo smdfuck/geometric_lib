aa095bba81b7d4c745be780cc62e0234fa0e9c71
Daniil Ulyanov <smdfuck@icloud.com>
2024-10-31 16:21:46 +0300

    add tests for square.py

---
+++ square_test.py
@@ -0,0 +1,19 @@
+import unittest
+import square
+
+class SquareTestCase(unittest.TestCase):
+    def test_square_zero_area(self):
+        res = square.area(0)
+        self.assertEqual(res, 0)
+
+    def test_square_area(self):
+        res = square.area(2)
+        self.assertEqual(res, 4)
+
+    def test_square_zero_perimeter(self):
+        res = square.perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_square_perimeter(self):
+        res = square.perimeter(2)
+        self.assertEqual(res, 8)
