e2515d4e8aed4dcaa571cc7e3a36f734d62e883b
Daniil Ulyanov <smdfuck@icloud.com>
2024-10-31 16:03:41 +0300

    test

---
+++ circle.py
@@ -0,0 +1,14 @@
+import math
+'''Import module math that keeps math constants and formulas'''
+
+def area(r):
+    '''Function get r radius of circle and return 2 * math.pi(3,14) * r^2
+        - area'''
+    return math.pi * r * r
+
+
+def perimeter(r):
+    '''Function get r radius and return 2 * math.pi(3,14) * r
+        - perimeter of circle'''
+    return 2 * math.pi * r
+
