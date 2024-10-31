81d35216b8302a20229e348412272ddb899e116c
Daniil Ulyanov <smdfuck@icloud.com>
2024-10-31 16:03:41 +0300

    fix rebase conflict

--- circle.py
+++ circle.py
@@ -1,10 +1,13 @@
 import math
 
-
 def area(r):
+    '''Function get r radius of circle and return 2 * math.pi(3,14) * r^2
+        - area'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Function get r radius and return 2 * math.pi(3,14) * r
+        - perimeter of circle'''
     return 2 * math.pi * r
 
