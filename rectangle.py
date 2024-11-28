7d9a0fcd186893fccc219725ef9669b20120fec4
Daniil Ulyanov <smdfuck@icloud.com>
2024-11-29 01:40:15 +0300

    fix all functions

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,12 @@
 def area(a, b): 
     '''Function get a, b sides of rectangle and return a*b
         - area'''
+    if a < 0 or b < 0: return 0
     return a * b
 
 def perimeter(a, b):
     '''Function get a, b sides and return 2*(a+b)
         - perimeter of rectangle'''
+    if a < 0 or b < 0: return 0
+    if a == 0 or b == 0: return 0
     return 2 * (a + b)
--- square.py
+++ square.py
@@ -1,10 +1,12 @@
 def area(a):
     '''Function get a side and return a^2
         - area of square'''
+    if a < 0: return 0
     return a * a
 
 
 def perimeter(a):
     '''Functin get a side and return 4*a
         - perimeter of square'''
+    if a < 0: return 0
     return 4 * a
